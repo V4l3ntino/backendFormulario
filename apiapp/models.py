36afbbb3f40168d7afdb5ddcbb0f484324934b38
vhondos <vhondos126@g.educaand.es>
2024-10-21 13:58:39 +0200

    .

--- apiapp/models.py
+++ apiapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Trabajador(models.Model):
+    id = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
     experiencia = models.IntegerField()
