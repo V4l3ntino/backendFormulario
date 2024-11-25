54a7fc538740d121683ec4bc528273c4858b560d
vhondos <vhondos126@g.educaand.es>
2024-11-25 12:43:50 +0100

    Id del trabajador en formato string

--- apiapp/models.py
+++ apiapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Trabajador(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=12)
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
     experiencia = models.IntegerField()
